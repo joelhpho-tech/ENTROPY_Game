6c0071141973aa4c1f06f35569a7ddcc085c731b
timothyclark5628-creator <timothyclark5628@gmail.com>
2026-02-27 15:27:24 -0500

    Imported impact WAVs + additional functionality
    The 6 impact wavs in the drive have been imported to the project and the floating object audio script has been given more functionality including pitch and volume variance.

--- Assets/Scripts/Audio/FloatingImpactAudio.cs
+++ Assets/Scripts/Audio/FloatingImpactAudio.cs
@@ -10,13 +10,21 @@ public class FloatingImpactAudio : MonoBehaviour
     private float oneFrameBackVelocity = 0;
     private float velocityDiff;
     [SerializeField]
-    private float velocityChangeFloor;
+    private float velocityChangeSoftFloor;
     [SerializeField]
     private float velocityChangeHardFloor;
+    [SerializeField]
+    private float minPitchShift;
+    [SerializeField]
+    private float maxPitchShift;
 
     [Header("Wall Bounce SFX")]
-    public AudioClip softBounce;
-    public AudioClip hardBounce;
+    public AudioClip solidPlasticSoftBounce;
+    public AudioClip solidPlasticHardBounce;
+    public AudioClip plasticBagSoftBounce;
+    public AudioClip plasticBagHardBounce;
+    public AudioClip metalBoxSoftBounce;
+    public AudioClip metalBoxHardBounce;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,11 +32,15 @@ public class FloatingImpactAudio : MonoBehaviour
         floatingAudioSource = this.gameObject.GetComponent<AudioSource>();
     }
 
+
     // Update is called once per frame
     void FixedUpdate()
     {
         velocity = rb.linearVelocity.magnitude;
         velocityDiff = Mathf.Abs(velocity - oneFrameBackVelocity);
+        //this will make the volume change depending on velocity
+        //volume (on an audio source) does not go higher than 1, which is normal volume, so the 'default' is it is quieter than normal.
+        floatingAudioSource.volume = velocity;
 
         //if the bump sound is not on cool down
         if (!bumpSoundOnCoolDown)
@@ -36,22 +48,28 @@ public class FloatingImpactAudio : MonoBehaviour
             //if the object experiences a change in velocity extreme enough to play a hard bounce sound...
             if (velocityDiff > velocityChangeHardFloor)
             {
-                Debug.Log(velocityDiff);
-                floatingAudioSource.clip = hardBounce;
-                floatingAudioSource.Play();
-                StartCoroutine(bumpCooldown());
+                randomizeAudio(); //randomize the pitch between the min and max values
+                floatingAudioSource.clip = metalBoxHardBounce; //set sound to play to be the hard bounce sound
+                floatingAudioSource.Play(); //play the sound
+                StartCoroutine(bumpCooldown()); //dont play another sound for a bit
             }
-            else if (velocityDiff > velocityChangeFloor) //if the change is only enough to play a soft bounce sound...
+            else if (velocityDiff > velocityChangeSoftFloor) //if the change is only enough to play a soft bounce sound...
             {
-                Debug.Log(velocityDiff);
-                floatingAudioSource.clip = softBounce;
-                floatingAudioSource.Play();
-                StartCoroutine(bumpCooldown());
+                randomizeAudio(); //randomize the pitch between the min and max values
+                floatingAudioSource.clip = metalBoxSoftBounce; //set sound to play to be the hard bounce sound
+                floatingAudioSource.Play(); //play the sound
+                StartCoroutine(bumpCooldown()); //dont play another sound for a bit
             }
         }
 
         //make sure this happens at the very end of FixedUpdate, or at minimum after it is checked against
-        oneFrameBackVelocity = rb.linearVelocity.magnitude;
+        oneFrameBackVelocity = rb.linearVelocity.magnitude; //replace with velocity?
+    }
+
+    private void randomizeAudio()
+    {
+        var randPitch = Random.Range(minPitchShift, maxPitchShift);
+        floatingAudioSource.pitch = randPitch;
     }
 
     private IEnumerator bumpCooldown()
@@ -61,6 +79,7 @@ public class FloatingImpactAudio : MonoBehaviour
         bumpSoundOnCoolDown = false;
     }
 
+    //this function is not called within the script, it is called in the logic for throwing the object
     public IEnumerator unmuteAfterTime()
     {
         yield return new WaitForSeconds(0.2f);
